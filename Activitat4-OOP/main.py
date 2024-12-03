e6787ea246b300e9d5b60c36854beefb80c930e5
ldloler <marc.be.pi@gmail.com>
2024-12-03 09:59:43 +0100

    Final classe 3/12

---
+++ Activitat4-OOP/main.py
@@ -0,0 +1,14 @@
+# Impotem les classes
+import cotxe
+import colibri
+
+# Creem 3 instancies de cotxe
+cotxe1 = cotxe("Ford", "Mustang", 3, 5, "Gasolina")
+cotxe2 = cotxe("Toyota", "Ralf 4", 5, 5, "Hybrid")
+cotxe3 = cotxe("Ferrari", "La Ferrari", 3, 2, "Gasolina")
+
+# Creem 3 instancies de colibri
+colibri1 = colibri() 
+colibri2 = colibri()
+colibri3 = colibri()
+# Fer altres atributs per colibri!!!!!
